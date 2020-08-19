0c809efef49720d1ef63f9f9ce9e3a1b7070e304
Carter Bouley <carterbouley@gmail.com>
2020-08-19 14:35:47 +0100

    deleting commit history

---
+++ generator/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import random
+# import request
+
+# Create your views here.
+def home(request):
+    return render(request, 'generator/home.html', {'password':'hialkj'})
+
+def password(request):
+
+
+    characters = list('abcdefghijklmnopqrstuvwxyz')
+
+    length = int(request.GET.get('length', 12))
+    #
+    if request.GET.get('uppercase'):
+        characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+
+    if request.GET.get('special'):
+        characters.extend(list('!@£$%^&*()'))
+    if request.GET.get('numbers'):
+        characters.extend(list('1234567890'))
+
+    # length = int(request.GET.get('length', 12))
+
+    thepassword = ''
+
+    for x in range(length):
+        thepassword += random.choice(characters)
+
+    return render(request, 'generator/password.html', {'password':thepassword})
+
+
+def about(request):
+    return render(request, 'generator/about.html')
